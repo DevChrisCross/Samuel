2c161a7c7e1fa2983310f0bd69b3284db81cbb6c
DevChrisCross <devchrismolina@gmail.com>
2018-02-14 02:01:26 +0800

    added multiprocessing support for translator:

--- samuel/topic_modeller.py
+++ samuel/topic_modeller.py
@@ -4,7 +4,6 @@ from gensim.corpora import Dictionary
 import pyLDAvis.gensim
 import json
 import gensim
-
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
 
@@ -28,76 +27,85 @@ def TextTopicModeller(normalized_text, visualize=False):
     else:
         return lda_model.get_topics()
 
-# lda = topic_modelling(corpus['normalized'], True)
-# def __extract_keyphrase(text, n_gram=2, keywords=4, correct_sent=False, tokenize_sent=True):
-#         """Extracts significant keywords or keyphrases that represents the idea of the entire text
+
+if __name__ == "__main__":
+    text3 = "I have missed iOS a lot over the last 3 years and it feels good to be back. Super smooth and no hiccups. I " \
+            "know few people have experienced some bugs recently. I guess I was one of the lucky ones not to have any " \
+            "issues. Maybe it was already patched when I bought the phone. However, my only complaint is the fact that " \
+            "iOS still does not let you clear all notifications from a particular app at once. I really would like to see " \
+            "fixed in a future update. Customisation wise, I do not have any issues because I hardly customised my Note " \
+            "4. Only widgets I had running were weather and calendar. Even then, I would still open up the actual app to " \
+            "seek more detail. However, I still do not use iCloud. I really wish Apple would have given us more online " \
+            "storage for backup. 5GB is hardly enough these days to backup everything on your phone. One of my mates " \
+            "suggested that Apple should ship the phone with iCloud storage same as the phone. It surely would be " \
+            "awesome. But business wise, I cannot see Apple going ahead with such a decision. But in my opinion, " \
+            "iCloud users should get at least 15GB free. Coming from an Android, I thought it would make sense to keep " \
+            "using my Google account to sync contacts and photos as it would take away the hassle of setting everything " \
+            "up from scratch. Only issue is sometimes I feel like iOS restricting the background app refresh of Google " \
+            "apps such as Google photos. For example, I always have to keep Google Photos running in order to allow " \
+            "background upload, which makes no sense. Same goes for OneDrive. Overall, navigation around the OS is easy " \
+            "and convenient. "
+
+# def __extract_keyphrase(text, n_gram=2, keywords=4):
 #
-#         :param text: string. the text to bextracted the info from
-#         :param n_gram: the gram model to be used for collocations
-#         :param keywords: the top number of keywords to return
-#         :param correct_sent: parameters for the normalize module
-#         :param tokenize_sent: parameters for the normalize module
-#         :return: an array of keyphrases
-#         """
+#     def word_similarity(words1, words2):
+#         words1 = [letter.lower() for letter in words1 if letter != " "]
+#         words2 = [letter.lower() for letter in words2 if letter != " "]
 #
-#         def word_similarity(words1, words2):
-#             words1 = [letter.lower() for letter in words1 if letter != " "]
-#             words2 = [letter.lower() for letter in words2 if letter != " "]
+#         words = words1 + words2
+#         character_vector = {i: {letter.lower(): 0 for letter in words} for i in range(2)}
+#         for index, word in enumerate([words1, words2]):
+#             for letter in word:
+#                 character_vector[index][letter] += 1
 #
-#             words = words1 + words2
-#             character_vector = {i: {letter.lower(): 0 for letter in words} for i in range(2)}
-#             for index, word in enumerate([words1, words2]):
-#                 for letter in word:
-#                     character_vector[index][letter] += 1
+#         c_vector = [[character_vector[i][letter] for letter in character_vector[i]] for i in range(2)]
+#         similarity_n = np.dot(c_vector[0], c_vector[1])
+#         similarity_d = np.sqrt(sum(np.square(c_vector[0]))) * np.sqrt(sum(np.square(c_vector[1])))
+#         similarity_score = similarity_n / similarity_d
 #
-#             c_vector = [[character_vector[i][letter] for letter in character_vector[i]] for i in range(2)]
-#             similarity_n = np.dot(c_vector[0], c_vector[1])
-#             similarity_d = np.sqrt(sum(np.square(c_vector[0]))) * np.sqrt(sum(np.square(c_vector[1])))
-#             similarity_score = similarity_n / similarity_d
+#         return similarity_score
 #
-#             return similarity_score
+#     sentences = Normalize.normalize_text(text, None, tokenize_sent, correct_sent)
+#     tf = __term_frequency(sentences["normalized"], n_gram)
+#     word_dict = {word: 0 for word in tf["word_dictionary"]}
+#     for sentence in tf["word_vector"]:
+#         for word in tf["word_vector"][sentence]:
+#             word_dict[word] += tf["word_vector"][sentence][word]
+#     collocations = sorted(word_dict.items(), key=lambda word: word[1], reverse=True)
+#     collocations = collocations[:keywords]
 #
-#         sentences = Normalize.normalize_text(text, None, tokenize_sent, correct_sent)
-#         tf = __term_frequency(sentences["normalized"], n_gram)
-#         word_dict = {word: 0 for word in tf["word_dictionary"]}
-#         for sentence in tf["word_vector"]:
-#             for word in tf["word_vector"][sentence]:
-#                 word_dict[word] += tf["word_vector"][sentence][word]
-#         collocations = sorted(word_dict.items(), key=lambda word: word[1], reverse=True)
-#         collocations = collocations[:keywords]
+#     raw_sentences = nltk.sent_tokenize(text) if tokenize_sent else text
+#     tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in raw_sentences]
+#     tagged_sentences = nltk.pos_tag_sents(tokenized_sentences)
+#     chunked_sentences = list(nltk.ne_chunk_sents(tagged_sentences))
 #
-#         raw_sentences = nltk.sent_tokenize(text) if tokenize_sent else text
-#         tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in raw_sentences]
-#         tagged_sentences = nltk.pos_tag_sents(tokenized_sentences)
-#         chunked_sentences = list(nltk.ne_chunk_sents(tagged_sentences))
+#     sc_regex_string = "[{}]".format(re.escape(string.punctuation))
+#     sc_regex_compiled = re.compile(pattern=sc_regex_string)
+#     word = 0
+#     tag = 1
+#     formed_noun = ""
 #
-#         sc_regex_string = "[{}]".format(re.escape(string.punctuation))
-#         sc_regex_compiled = re.compile(pattern=sc_regex_string)
-#         word = 0
-#         tag = 1
-#         formed_noun = ""
+#     phrase_tag_list = ["DT", "JJ", "NN", "NNS", "NNP", "NNPS"]
+#     noun_phrases = list()
+#     for i in range(len(chunked_sentences)):
+#         for j in range(len(chunked_sentences[i])):
+#             chunked_word = chunked_sentences[i][j]
+#             if hasattr(chunked_word, "label"):
+#                 formed_noun += (" " if formed_noun else "") + (" ".join([child[0] for child in chunked_word]))
+#             else:
+#                 is_special_character = sc_regex_compiled.sub(string=chunked_word[word], repl="") == ""
+#                 if not is_special_character or chunked_word[word] == "." or chunked_word[word] == ",":
+#                     if chunked_word[tag] in phrase_tag_list:
+#                         formed_noun += (" " if formed_noun else "") + chunked_word[word]
+#                     else:
+#                         if formed_noun:
+#                             noun_phrases.append(formed_noun)
+#                             formed_noun = ""
 #
-#         phrase_tag_list = ["DT", "JJ", "NN", "NNS", "NNP", "NNPS"]
-#         noun_phrases = list()
-#         for i in range(len(chunked_sentences)):
-#             for j in range(len(chunked_sentences[i])):
-#                 chunked_word = chunked_sentences[i][j]
-#                 if hasattr(chunked_word, "label"):
-#                     formed_noun += (" " if formed_noun else "") + (" ".join([child[0] for child in chunked_word]))
-#                 else:
-#                     is_special_character = sc_regex_compiled.sub(string=chunked_word[word], repl="") == ""
-#                     if not is_special_character or chunked_word[word] == "." or chunked_word[word] == ",":
-#                         if chunked_word[tag] in phrase_tag_list:
-#                             formed_noun += (" " if formed_noun else "") + chunked_word[word]
-#                         else:
-#                             if formed_noun:
-#                                 noun_phrases.append(formed_noun)
-#                                 formed_noun = ""
+#     formed_keyphrases = dict()
+#     for keyphrase in collocations:
+#         top_phrases = {phrase: word_similarity(keyphrase[0], phrase) for phrase in noun_phrases}
+#         top_phrases = sorted(top_phrases.items(), key=lambda word: word[1], reverse=True)
+#         formed_keyphrases[keyphrase[0]] = top_phrases[0]
 #
-#         formed_keyphrases = dict()
-#         for keyphrase in collocations:
-#             top_phrases = {phrase: word_similarity(keyphrase[0], phrase) for phrase in noun_phrases}
-#             top_phrases = sorted(top_phrases.items(), key=lambda word: word[1], reverse=True)
-#             formed_keyphrases[keyphrase[0]] = top_phrases[0]
-#
-#         return formed_keyphrases
+#     return formed_keyphrases
--- samuel/translator.py
+++ samuel/translator.py
@@ -1,5 +1,7 @@
 from googletrans import Translator
 from enum import Enum
+from functools import partial
+import multiprocessing as mp
 
 translator = Translator(service_urls=[
     'translate.google.com',
@@ -13,3 +15,42 @@ class Language(Enum):
 
 def translate(corpus: str, enabled=False, translate_from=Language.TAGALOG.value, translate_to=Language.ENGLISH.value):
     return corpus if not enabled else translator.translate(corpus, translate_to, translate_from).text
+
+
+class TranslatorManager:
+    def __init__(self, text: str, character_threshold: int = 14500,
+                 translate_from: Language = Language.TAGALOG.value, translate_to: Language = Language.ENGLISH.value):
+        tokens = text.split()
+
+        def partition_text():
+            batch_string = ""
+            for token in tokens:
+                if len(batch_string) > character_threshold:
+                    yield batch_string
+                    batch_string = token
+                else:
+                    batch_string += " " + token
+            yield batch_string
+
+        print("Preparing document batches: object", (id(self)))
+        text_batches = list(partition_text())
+
+        print("Preparing process pool: object", (id(self)))
+        pool = mp.Pool()
+        print("Mapping document batches: object", (id(self)))
+        result = pool.map_async(partial(translate, translate_from=translate_from, translate_to=translate_to),
+                                text_batches)
+
+        self._text = " ".join(result.get())
+        pool.close()
+        pool.join()
+        print("Translation pooling done: object", (id(self)))
+
+    @property
+    def translated_text(self):
+        return self._text
+
+
+if __name__ == "__main__":
+    from samuel.test.test_document import single_test_document
+    print(TranslatorManager(single_test_document).translated_text)
